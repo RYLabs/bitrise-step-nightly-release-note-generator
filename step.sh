105d28ec5389900f12ed63c4e539fd1d6c6803b1
Randy Etheredge <eman8519@gmail.com>
2018-08-18 00:45:43 -0400

    Adding better logging.

--- step.sh
+++ step.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -ex
 
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 project_folder=$BITRISE_SOURCE_DIR
 
 cd $project_folder
@@ -11,14 +14,17 @@ LATEST_NIGHTLY_TAG=${CURRENT_NIGHTLY_TAGS[0]}
 
 date=$(date '+%m-%d-%Y')
 
-[[ -z "$LATEST_NIGHTLY_TAG" ]] && { git tag "$NIGHTLY_BUILD_TAG_FORMAT-$date" && echo "No nightly tags found. Exiting"; exit 1; }
+[[ -z "$LATEST_NIGHTLY_TAG" ]] && { git tag "$NIGHTLY_BUILD_TAG_FORMAT-$date" && echo "No nightly tags found. Exiting" >&2; exit 1; }
+
+echo "Latest nightly tag found: $LATEST_NIGHTLY_TAG" >&1
 
 RAW_MERGE_LOG=$(git log $LATEST_NIGHTLY_TAG..HEAD --merges)
 
-[[ -z "$RAW_MERGE_LOG" ]] && { echo "No changes detected from last nightly build. Exiting"; exit 1; }
+[[ -z "$RAW_MERGE_LOG" ]] && { echo "No changes detected from last nightly build. Exiting" >&2; exit 1; }
 
 JIRA_TICKETS_ADDRESSED=$(echo $RAW_MERGE_LOG | grep -o '[A-Z][A-Z0-9]\+-[0-9]\+' | awk '!a[$0]++' )
 
+echo -e "${GREEN}Writing out release_notes.md${NC}" >&1
 cat > release_notes.md <<EOF
 Nightly Build $date
 =================================
@@ -33,9 +39,11 @@ Raw Merge Log:
 $(while read -r merge_log ; do echo "	$merge_log"; done <<< "$(git log $LATEST_NIGHTLY_TAG..HEAD --merges --oneline)")
 EOF
 
+echo -e "Removing previous nightly tag." >&1
 git tag -d $LATEST_NIGHTLY_TAG
 
+echo -e "Writing new nightly tag." >&1
 git tag "$NIGHTLY_BUILD_TAG_FORMAT-$date"
 
-
+echo -e "${GREEN}Exporting release_notes.md.${NC}" >&1
 envman add --key RELEASE_NOTES_PATH --value '$project_folder/release_notes.md'
